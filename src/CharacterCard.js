4a27d745fd9d61f3c72393c8bf46116704b19243
someoneCoe <jom.namom@gmail.com>
2019-09-26 12:16:10 +0700

    first commit

---
+++ src/CharacterCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class CharacterCard extends Component {
+    constructor(props){
+    super(props)
+    this.state = {
+    active: false,
+    }
+    }
+    componentDidUpdate(prevProps){
+        if(prevProps.attempt != this.props.attempt){
+        this.setState({active: false})
+        }
+       }
+    activate = () => {
+        if(!this.state.active){
+        this.props.activationHandler(this.props.value)
+        this.setState({active: true})
+        }
+       }
+       
+    render(){
+    let className = `card ${this.state.active ? 'activeCard': ''}`
+    return (
+    <div className={className} onClick={this.activate}>
+    {this.props.value}
+    </div>
+    )
+    }
+   }
